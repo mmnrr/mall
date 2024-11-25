ada12fc7dc9d16a78aa721264d73e47e6c623508
D_wW <about1201@gmail.com>
2024-11-25 14:07:58 +0900

    D1_init basement

---
+++ src/layouts/BasicLayout.js
@@ -0,0 +1,28 @@
+import BasicMenu from "../components/menus/BasicMenu";
+
+const BasicLayout = ({children}) => {
+    return (
+    <>
+    
+    {/* <header className="bg-teal-400 p-5">
+        <h1 className="text-2xl md:text-4xl">Header</h1>
+    </header> */}
+    <BasicMenu></BasicMenu>
+
+    
+    <div className="bg-white my-5 w-full flex flex-col space-y-1 md:flex-row md:space-x-1 md:space-y-0">
+
+    <main className="bg-sky-300 md:w-2/3 lg:w-3/4 px-5 py-5">
+        <h1 className="text-2xl md:text-4xl">{children}</h1>
+    </main>
+
+    <aside className="bg-green-300 md:w-1/3 lg:w-1/4 px-5 py-5">
+        <h1 className="text-2xl md:text-4xl"> Sidebar </h1>
+    </aside>
+    </div>
+    
+    </>
+    );
+    }
+
+    export default BasicLayout;
